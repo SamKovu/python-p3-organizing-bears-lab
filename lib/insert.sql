d2b8b3cff6e3be16a6ee3e6d8740b53be1a9e7f4
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-16 00:43:57 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr.Chcolate",5,"M","Brown","Friendly",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy",1,"M","Black","Placid",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha",3,"F","Brown","Angry",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",5,"M","Brown","Friendly",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa",13,"F","White","Friendly",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch",9,"M","Brown","Vicious",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy",7,"F","Black&White","Friendly",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,16,"M","Brown","Friendly",1);
